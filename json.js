f6acab5658f1c2ba30d8758d4e3327da7627e4b9
hchaichiiheb <165536493+hchaichiiheb@users.noreply.github.com>
2024-04-26 11:13:55 +0100

    Create json.js

---
+++ json.js
@@ -0,0 +1,37 @@
+let questions = 
+[
+    {
+        numb: 1,
+        answer: "C.hyper text Markup Language",
+        question: "what does HTML stand for?",
+        options:[ "A.hyper type multi language","B.hyper text multiple language","C.hyper text Markup Language","D.home text multi language"                                                        ]
+    },
+    {
+    numb: 2,
+    answer: "A.cascading Style sheet",
+    question: "what does CSS stand for?",
+    options: [ "A.cascading Style sheet","B.cute style sheet","C.Computer Style sheet","D.Codehal Style sheet"]
+
+    },
+    {
+    numb: 3,
+    answer: "A.hypertext Preprocessor",
+    question: "what does PHP stand for ?",
+    options:["A.hypertext Preprocessor","B.Hometext Programming","C.Hypertext Programming","D.Programming Hypertext Preprocessor"                 ]
+    
+    },
+    {
+    numb: 4,
+    answer: "D. structured Querry Language",
+    question: "what does sql stand for ? ",
+    options:[ "A. Strength Query language ","B. stylesheet Query language","C. science question language","D. structured Querry Language"        ]
+    },
+    {  
+        numb: 5,
+        answer:"D. Extensible Markup Language",
+        question:"What does XML stand for ?",
+        options: ["A. Excellent Multiple Language","B.Explore Multiple language  ","Extra markup language","D. Extensible Markup Language"            ]
+       
+    
+    }
+];
